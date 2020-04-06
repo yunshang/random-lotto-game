8f64ef3ed960bfb6236e38bd3a45a200c956a593
yunshang <alshin@126.com>
2020-04-06 18:39:53 +0800

    Updates

---
+++ precache-manifest.f962ca7342a6990a32b44b4c9effbc5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b86183c76c8f431183c2d3de04dddb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "290dd4ff6507a6d1d6dd",
+    "url": "/static/css/2.345148b5.chunk.css"
+  },
+  {
+    "revision": "520b8484daff964b71d1",
+    "url": "/static/css/main.058a4fcb.chunk.css"
+  },
+  {
+    "revision": "290dd4ff6507a6d1d6dd",
+    "url": "/static/js/2.6c2176c6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6c2176c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520b8484daff964b71d1",
+    "url": "/static/js/main.38071efd.chunk.js"
+  },
+  {
+    "revision": "d87cf153ad0b1791f62d",
+    "url": "/static/js/runtime-main.89a4436d.js"
+  }
+]);
