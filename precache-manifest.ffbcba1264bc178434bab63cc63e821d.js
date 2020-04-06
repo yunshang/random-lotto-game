d61bcb54b7402173fbf5d389c4ef1bf4e4a1837f
yunshang <alshin@126.com>
2020-04-06 19:11:02 +0800

    Updates

---
+++ precache-manifest.ffbcba1264bc178434bab63cc63e821d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8055ff2ebf373dafc04b2f39961c63a5",
+    "url": "/random-lotto-game/index.html"
+  },
+  {
+    "revision": "290dd4ff6507a6d1d6dd",
+    "url": "/random-lotto-game/static/css/2.345148b5.chunk.css"
+  },
+  {
+    "revision": "e8e7bf7818e4def9e62d",
+    "url": "/random-lotto-game/static/css/main.058a4fcb.chunk.css"
+  },
+  {
+    "revision": "290dd4ff6507a6d1d6dd",
+    "url": "/random-lotto-game/static/js/2.6c2176c6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/random-lotto-game/static/js/2.6c2176c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e7bf7818e4def9e62d",
+    "url": "/random-lotto-game/static/js/main.d3c8c673.chunk.js"
+  },
+  {
+    "revision": "d901cf2c7d96a8465b46",
+    "url": "/random-lotto-game/static/js/runtime-main.1a0a1ea6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f962ca7342a6990a32b44b4c9effbc5e.js"
+  "/random-lotto-game/precache-manifest.ffbcba1264bc178434bab63cc63e821d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/random-lotto-game/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
